a1d0d06234caa8911ef23cf907c507ec3e6b6703
Idris mukthar <idrismukthar6@gmail.com>
2025-05-20 22:45:22 +0100

    Add first C++ code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world \n\n";
+    cout << "My name is balogun \n";
+    cout << 5 << "\n";      // added \n
+    cout << 3 + 7 << "\n";  // added \n
+    cout << 5 * 10 << "\n"; // added \n
+    cout << 100 / 99 << "\n";
+    int a = 5;
+    int b = 7;
+    int sum = a + b;
+    cout << "Sum is: " << sum << "\n"; // added \n
+    return 0;
+}
